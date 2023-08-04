8908063ebd994641cd46f766c33b85e573474e97
Student01 <Student01@DINEO123>
2023-08-04 09:58:15 +0200

    Add project files.

---
+++ CreatingAnInterface/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CreatingAnInterface
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            VWAmarok a = new VWAmarok();
+            VWPolo polo = new VWPolo();
+            VWGTI gti = new VWGTI();
+
+            a.paint();
+            polo.Badge();
+            gti.polish();
+           
+        }
+        public class VWPolo:IControls
+        {
+            public void Badge()
+            {
+
+            }
+
+            public void infortainment()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void inspection()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void paint()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void polish()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void wheels()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class VWAmarok:IControls
+        {
+            public void Badge()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void infortainment()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void inspection()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void paint()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void polish()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void wheels()
+            {
+
+            }
+           
+        }
+        public class VWGTI:IControls
+        {
+            public void Badge()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void infortainment()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void inspection()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void paint()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void polish()
+            {
+
+            }
+
+            public void wheels()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        interface IControls
+        {
+            void Badge();
+            void infortainment();
+            void wheels();
+            void inspection();
+            void paint();
+            void polish();
+        }
+    }
+}
